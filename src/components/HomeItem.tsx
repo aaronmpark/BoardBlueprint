a2d6c0bb2b3934c9a084e807760980c7e7b46da3
Aaron Park <aaronprk2004@gmail.com>
2024-03-02 12:47:00 -0500

    deleted unnecessary code for the Home page

--- src/components/HomeItem.tsx
+++ src/components/HomeItem.tsx
@@ -45,27 +45,7 @@ export function HomeItem({ id, name, imgUrl }:
                 <span className="fs-2">{name}</span>
             </Card.Title>
             <div className="mt-auto">
-                {quantity === 0 ? (
-                    <Button className="w-100" onClick={() => navigateSite(name)}>+ Add</Button>
-                ) : <div className="d-flex align-items-center 
-                flex-column" style={{ gap: ".5rem" }}>
-                    <div className="d-flex align-items-center 
-                    justify-content-center" style={{ gap: ".5rem" }}>
-
-                        <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
-                        <div>
-                            <span className="fs-3">{quantity}</span> in cart
-                        </div>
-                        <Button onClick={() => increaseCartQuantity(id)}>+</Button>
-                    </div>
-                    <Button onClick={() => removeFromCart(id)}
-                        variant="danger"
-                        size="sm"
-                    >
-                        Remove
-                    </Button>
-
-                </div>}
+                <Button className="w-100" onClick={() => navigateSite(name)}>+ Add</Button>
             </div>
         </Card.Body>
     </Card>
