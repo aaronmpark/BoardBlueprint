ac7940d6dd4b80b9cecce173c3d02ad741e1b05d
Jordan Tran <jordntran.t@gmail.com>
2024-03-02 02:19:09 -0500

    home page update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,16 @@
+import { Col, Row } from "react-bootstrap"
+import homeItems from "../data/keyboard.json"
+import { HomeItem } from "../components/HomeItem"
+
 export function Home() {
-    return <h1>Home</h1>
+    return <><h1>Home</h1>
+        <Row md={2} xs={1} lg={3} className="g-3">
+            {homeItems.map(item => (
+                <Col key={item.id}>
+                    <HomeItem {...item} />
+                </Col>
+            ))}
+        </Row>
+
+    </>
 }
