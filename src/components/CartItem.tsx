bd0a76ba4f684654fb3d51016aca058435f4dc31
Aaron Park <aaronprk2004@gmail.com>
2024-03-02 00:01:07 -0500

    test to create branch

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -3,6 +3,10 @@ import { useShoppingCart } from "../context/ShoppingCartContext"
 import storeItems from "../data/items.json"
 import { formatCurrency } from "../utilities/formatCurrency"
 
+// items found within the cart 
+
+//csName="me-auto" -> automatically moves the stuff to the  right
+
 
 type CartItemProps = {
     id: number
