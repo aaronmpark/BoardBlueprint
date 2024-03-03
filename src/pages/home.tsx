497629714434da011259f20d08557d5db9662d98
Jordan Tran <jordntran.t@gmail.com>
2024-03-03 00:51:18 -0500

    added copyright at the bottom of home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,18 +3,44 @@ import homeItems from '../data/keyboard.json'
 import { HomeItem } from '../components/HomeItem'
 
 export function Home() {
-    return ( <> <h1 className = "mt-3">Home</h1>
-            <Container>
-                <Row className="mt-3">
-                    <Col xs={12} md={4} className="g-3">
-                        <ListGroup>
-                            {homeItems.map(item => (
-                                <HomeItem key={item.id} {...item} />
-                            ))}
-                        </ListGroup>
-                    </Col>
-                </Row>
-            </Container>
+    return (
+        <>
+            <style>{`
+                .page-container {
+                    min-height: 100vh;
+                    display: flex;
+                    flex-direction: column;
+                }
+                .content-wrap {
+                    flex: 1;
+                }
+                .footer {
+                    width: 100%;
+                    background: #ffffff;
+                    color: black;
+                    text-align: center;
+                    padding: 25px 0;
+                }
+            `}</style>
+            <div className="page-container">
+                <div className="content-wrap">
+                    <Container>
+                        <h1 className="mt-3">Keyboards</h1>
+                        <Row className="mt-3">
+                            <Col xs={12} md={4} className="g-3">
+                                <ListGroup>
+                                    {homeItems.map(item => (
+                                        <HomeItem key={item.id} {...item} />
+                                    ))}
+                                </ListGroup>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+                <footer className="footer">
+                    &copy; {new Date().getFullYear()} Your Company. All rights reserved.
+                </footer>
+            </div>
         </>
     );
 }
