20c0b8b9af5e24410efdbd8447f210c3dfbe51e1
Aaron Park <aaronprk2004@gmail.com>
2024-03-31 03:22:04 -0400

    added about a tiny bit

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,28 @@
+import { Container, Row, Col } from 'react-bootstrap';
 import Copyright from "../components/Copyright";
 
 export function About() {
   return (
     <>
-      <h1 className="mt-4 mb-4">About</h1>
+      <Container className="mt-4 mb-4">
+        <Row className="justify-content-md-center">
+          <Col xs={12}>
+            <h1>About</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6} sm={12}>
+            <p>
+              Want to see more? .... skarner skarner skarner
+            </p>
+          </Col>
+          <Col md={6} sm={12}>
+            <p>
+              github here or some shit idk
+            </p>
+          </Col>
+        </Row>
+      </Container>
       <footer className="fixed-bottom">
         <Copyright />
       </footer>
